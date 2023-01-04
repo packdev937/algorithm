a3f88a3111b6a0264f1d27ca4b42e716b0ed0609
gundorit <packdev937@gmail.com>
2023-01-04 11:48:26 +0900

    add algorithm related to string

--- src/inflearn/string/_9.java
+++ src/inflearn/string/_9.java
@@ -4,18 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _1 {
+public class _9 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        str = str.toLowerCase();
-        char c = br.readLine().charAt(0);
-        int cnt = 0;
+        String temp = "";
 
-        for(char x : str.toCharArray()){
-            if(x == c) cnt++;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                temp += str.charAt(i);
+            }
         }
 
-        System.out.println(cnt);
+        System.out.println(Integer.parseInt(temp));
     }
 }
