c598a85edf5088041c9a2d9b50b7fa7037981b99
gundorit <packdev937@gmail.com>
2023-01-07 07:40:57 +0900

    add algorithm related to string

--- src/inflearn/string/_10.java
+++ src/inflearn/string/_10.java
@@ -3,33 +3,41 @@ package inflearn.string;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class _10 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        char c = sc.next().charAt(0);
-        StringTokenizer st = new StringTokenizer(str, Character.toString(c));
-
-        // 첫 번째는 토큰에 포함되지 않음
-        if(str.charAt(0) == c) System.out.print("0 ");
-
-        while (st.hasMoreTokens()) {
-            String temp = st.nextToken();
-            System.out.print(temp+"@@");
-            int len = temp.length();
-            int cnt = 1;
-            for (int i = 0; i < len / 2; i++) {
-                System.out.print(cnt++ + " ");
+import java.util.*;
+class _10 {
+    public int[] solution(String s, char t){
+        int[] answer=new int[s.length()];
+        int p=1000;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==t){
+                p=0;
+                answer[i]=p;
             }
-            if(len%2 ==0) cnt--;
-            for(int i = len/2; i<len; i++){
-                System.out.print(cnt-- + " ");
+            else{
+                p++;
+                answer[i]=p;
             }
+        }
+        p=1000;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==t) p=0;
+            else{
+                p++;
+                answer[i]=Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
 
-            System.out.print("0 ");
+    public static void main(String[] args){
+        _10 T = new _10();
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        char c=kb.next().charAt(0);
+        for(int x : T.solution(str, c)){
+            System.out.print(x+" ");
         }
     }
 }
-
 // 일단 e가 맨 앞이나 맨 뒤에 올 때 0을 추가해야 되고
 // 추가로 Token의 길이가 3일 때는 1 2 1 4 일 때는 1 2 2 1 5일 때는 1 2 3 2 1 이런 식으로 해야 함
--- src/inflearn/string/_8_1.java
+++ src/inflearn/string/_8_1.java
@@ -3,18 +3,12 @@ package inflearn.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class _8 {
+public class _8_1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " `~1@#$%^&*()-=_+,./<>?;':[]{}");
+        String str = br.readLine().toUpperCase().replaceAll("[^A-Z]", "");
         boolean isPelindrome = true;
-        String str = "";
-        while (st.hasMoreTokens()) {
-            str += st.nextToken();
-        }
-        str = str.toUpperCase();
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
                 isPelindrome = false;
@@ -25,6 +19,5 @@ public class _8 {
         if (isPelindrome)
             System.out.print("YES");
         else System.out.print("NO");
-
     }
 }
