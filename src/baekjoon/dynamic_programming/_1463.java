c864df01cabd7ff87f6e080e1ed81bf0e1f3f55b
gundorit <packdev937@gmail.com>
2023-05-18 22:20:03 +0900

    Solved Baekjoon problems

--- src/baekjoon/dynamic_programming/_1463.java
+++ src/baekjoon/dynamic_programming/_1463.java
@@ -1,9 +1,11 @@
 package baekjoon.dynamic_programming;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class _1463 {
-    static Integer[] dp; // 메모이제이션 할 배열
+    static Integer[] dp;
 
     static int recur(int N) {
 
@@ -28,10 +30,12 @@ public class _1463 {
         return dp[N];
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        dp[3] = 1;
-        dp[2] = 1;
-        System.out.println(recur(sc.nextInt()));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        dp = new Integer[N + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        System.out.println(recur(N));
     }
 }
