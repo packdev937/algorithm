fc5f0fa33822286a3937dfe7cfc4e4c3aa79d8ff
gundorit <packdev937@gmail.com>
2023-06-02 14:59:47 +0900

    solve (Baekjoon) : 10815 숫자 카드

---
+++ src/baekjoon/data_structure/_10815.java
@@ -0,0 +1,31 @@
+package baekjoon.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class _10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            map.put(Integer.parseInt(st.nextToken()), 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (map.containsKey(Integer.parseInt(st.nextToken())))
+                sb.append("1").append(" ");
+            else sb.append("0").append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
