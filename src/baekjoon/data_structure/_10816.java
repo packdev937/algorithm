3c57d1365b4d843675d1976edb6dc07aead8531b
gundorit <packdev937@gmail.com>
2023-06-02 12:14:18 +0900

    solve (Baekjoon) : 10816 숫자 카드 2

---
+++ src/baekjoon/data_structure/_10816.java
@@ -0,0 +1,37 @@
+package baekjoon.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class _10816 {
+
+    static HashMap<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (map.get(num) == null)
+                sb.append(0).append(" ");
+            else
+                sb.append(map.get(num)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
