bb8043d8dcd4f25281fa0d15515e7da1d55e8474
gundorit <packdev937@gmail.com>
2023-06-06 20:45:26 +0900

    solve (Baekjoon) : 27323

---
+++ src/baekjoon/implementation/_27323.java
@@ -0,0 +1,10 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+public class _27323 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() * sc.nextInt());
+    }
+}
