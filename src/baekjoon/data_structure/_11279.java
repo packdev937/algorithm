a8745dbc895d4940bfe7d5eb8aa4e9f281b60dee
gundorit <packdev937@gmail.com>
2023-06-25 12:18:11 +0900

    solve (Baekjoon) : 11279 최대 힙

---
+++ src/baekjoon/data_structure/_11279.java
@@ -0,0 +1,36 @@
+package baekjoon.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class _11279 {
+    static PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    });
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0 + "\n");
+                    continue;
+                }
+                sb.append(pq.poll() + "\n");
+                continue;
+            }
+            pq.add(input);
+        }
+        System.out.println(sb);
+    }
+}
