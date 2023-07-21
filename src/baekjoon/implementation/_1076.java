b787624093936962f2d2ccf8d21e3dc8c88af684
gundorit <packdev937@gmail.com>
2023-07-21 19:57:51 +0900

    solve (baekjoon) : 1076 저항

---
+++ src/baekjoon/implementation/_1076.java
@@ -0,0 +1,27 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+public class _1076 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int black = 0;
+        int brown = 1;
+        int red = 2;
+        int orange = 3;
+        int yellow = 4;
+        int green = 5;
+        int blue = 6;
+        int violet = 7;
+        int grey = 8;
+        int white = 9;
+
+        int[] multiply = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000,
+            1000000000};
+
+        for (int i = 0; i < 3; i++) {
+        }
+    }
+
+}
