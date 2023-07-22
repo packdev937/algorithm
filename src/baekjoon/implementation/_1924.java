0b9b5d2ece1e5843aa3a365816de28e9322d1d57
gundorit <packdev937@gmail.com>
2023-07-22 11:43:18 +0900

    solve (baekjoon) : 1924 2007년

--- src/baekjoon/implementation/_1924.java
+++ src/baekjoon/implementation/_1924.java
@@ -1,7 +1,45 @@
 package baekjoon.implementation;
 
-public class _1924 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class _1924 {
+
+    static int[] arr = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        int dayoff = 0;
+        for (int i = 1; i < M; i++) {
+            dayoff += arr[i - 1];
+        }
+        dayoff += D;
+
+
+        switch (dayoff % 7) {
+            case 0:
+                System.out.print("SUN");
+                break;
+            case 1:
+                System.out.print("MON");
+                break;
+            case 2:
+                System.out.print("TUE");
+                break;
+            case 3:
+                System.out.print("WED");
+                break;
+            case 4:
+                System.out.print("THU");
+                break;
+            case 5:
+                System.out.print("FRI");
+                break;
+            case 6:
+                System.out.print("SAT");
+                break;
+        }
     }
 }
