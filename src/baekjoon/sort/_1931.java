a4808328cbcc3cb211d3dd4530bcef359ab00653
gundorit <packdev937@gmail.com>
2023-08-06 20:41:29 +0900

    solve (baekjoon) : 1931 회의실 배정

--- src/baekjoon/sort/_1931.java
+++ src/baekjoon/sort/_1931.java
@@ -2,14 +2,16 @@ package baekjoon.sort;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
-class Room implements Comparable<Room> {
+class Conference implements Comparable<Conference> {
+
     int sTime;
     int eTime;
 
     @Override
-    public int compareTo(Room o) {
-        if (this.sTime == o.sTime) {
+    public int compareTo(Conference o) {
+        if (this.eTime - o.eTime != 0) {
             return this.eTime - o.eTime;
         }
         return this.sTime - o.sTime;
@@ -17,19 +19,45 @@ class Room implements Comparable<Room> {
 }
 
 public class _1931 {
+
+    static Stack<Conference> stack = new Stack<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        Room[] arr = new Room[N];
+        Conference[] arr = new Conference[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = new Room();
+            arr[i] = new Conference();
             arr[i].sTime = sc.nextInt();
             arr[i].eTime = sc.nextInt();
         }
 
         Arrays.sort(arr);
+        int idx = 0;
+        int cnt = 1;
+        stack.push(arr[idx++]);
+
+        while (idx < arr.length) {
+            if (arr[idx].sTime >= stack.peek().eTime) {
+                stack.push(arr[idx]);
+                cnt++;
+            }
+            idx++;
+        }
+        System.out.println(stack.size());
     }
 }
 
 // 회의실 배정
+//1 4
+//3 5
+//0 6
+//5 7
+//3 8
+//5 9
+//6 10
+//8 11
+//8 12
+//2 13
+//12 14
