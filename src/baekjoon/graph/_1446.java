8fb5255744c699e018864b2eb69d045202cf82f1
gundorit <packdev937@gmail.com>
2023-08-18 20:12:31 +0900

    solve (baekjoon) : 1446 지름길

--- src/baekjoon/graph/_1446.java
+++ src/baekjoon/graph/_1446.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class _1504 {
+public class _1446 {
 
     static PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
         @Override
