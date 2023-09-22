666ce533563a2e91e3429bd9248d76937b447850
gundorit <packdev937@gmail.com>
2023-09-22 23:55:28 +0900

    solve (baekjoon) : 2075 N번째 큰 수

---
+++ src/baekjoon/data_structure/priority_queue/_2075.java
@@ -0,0 +1,35 @@
+package baekjoon.data_structure.priority_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class _2075 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] board = new int[N][N];
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                queue.add(board[i][j]);
+            }
+        }
+        for (int i = 0; i < N - 1; i++) {
+            queue.poll();
+        }
+        System.out.println(queue.poll());
+    }
+
+}
