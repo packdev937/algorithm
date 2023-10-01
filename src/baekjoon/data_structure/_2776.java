31b8d75badc73fc64d77f7f625bac748142c23f5
gundorit <packdev937@gmail.com>
2023-10-01 21:39:22 +0900

    solve (baekjoon) : 2776 암기왕

---
+++ src/baekjoon/data_structure/_2776.java
@@ -0,0 +1,40 @@
+package baekjoon.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class _2776{
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer stringTokenizer;
+
+        int testCase = Integer.parseInt(br.readLine());
+        while(testCase-- > 0){
+            int N = Integer.parseInt(br.readLine());
+            Set<Integer> set = new HashSet<>();
+
+            stringTokenizer = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N ; i++){
+                set.add(Integer.parseInt(stringTokenizer.nextToken()));
+            }
+
+            int M = Integer.parseInt(br.readLine());
+            stringTokenizer = new StringTokenizer(br.readLine());
+            for(int i = 0 ; i < M ; i++){
+                int x = Integer.parseInt(stringTokenizer.nextToken());
+
+                if(set.contains(x))
+                    sb.append(1).append("\n");
+                else
+                    sb.append(0).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
