167d2c933378dd75ad08169e6acc4f5a70103aef
gundorit <packdev937@gmail.com>
2023-10-08 18:43:22 +0900

    solve (baekjoon) : 17219 비밀번호 찾기

---
+++ src/baekjoon/data_structure/_17219.java
@@ -0,0 +1,29 @@
+package baekjoon.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class _17219 {
+
+    static HashMap<String, String> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String input = br.readLine();
+            System.out.println(map.get(input));
+        }
+    }
+}
