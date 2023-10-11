db89a8bcd0f14e7a8c1bd72cce5d26dded88f103
gundorit <packdev937@gmail.com>
2023-10-11 19:03:32 +0900

    solve (baekjoon) : 1967 트리의 지름

---
+++ src/baekjoon/graph/_1967.java
@@ -0,0 +1,25 @@
+package baekjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _1967 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+        }
+    }
+
+}
+
+// 가중치가 있는 그래프를 어떻게 표현하는가?
+// int []?
