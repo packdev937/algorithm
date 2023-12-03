ce6c91298727fe61f235ce8478953fecc4065a0a
gundorit <packdev937@gmail.com>
2023-12-03 22:34:26 +0900

    solve (baekjoon) : 2636 치즈

--- src/baekjoon/graph/_2636.java
+++ src/baekjoon/graph/_2636.java
@@ -1,117 +1,139 @@
 package baekjoon.graph;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class _2636 {
+
     static int[][] board;
     static boolean[][] ch;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static final int air = 99999;
+    static int N, M;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
         board = new int[N][M];
         ch = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                board[i][j] = sc.nextInt();
+	board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int hr = 0;
-        int cnt = 0;
-        Queue<int[]> q = new LinkedList<>();
-        while (true) {
-            hr++;
-            if (isBoardEmpty())
-                break;
+        int hour = 0;
+        int lastCheese = 0;
+        while (!isBoardEmpty()) {
+            hour++;
             makeAir();
-            cnt = 0;
-            for (int i = 1; i < N; i++) {
-                for (int j = 1; j < M; j++) {
-                    boolean isTrue = false;
-                    for (int k = 0; k < 4; k++) {
-                        int ny = i + dy[k];
-                        int nx = j + dx[k];
-                        if (nx < board[0].length && ny < board.length && board[ny][nx] == air && board[i][j] == 1) {
-                            isTrue = true;
-                            break;
-                        }
-                    }
-                    if (isTrue && !ch[i][j]) {
-                        board[i][j] = air;
-                        q.add(new int[]{i, j});
-                        ch[i][j] = true;
-                    }
-                }
-            }
-            System.out.println(q.size());
-
-            while (!q.isEmpty()) {
-                int[] temp = q.poll();
-                board[temp[0]][temp[1]] = air;
-                cnt++;
+            int cnt = meltCheese();
+            if (cnt > 0) {
+	lastCheese = cnt;
             }
         }
-        System.out.println(hr);
-        System.out.println(cnt);
+        System.out.println(hour);
+        System.out.println(lastCheese);
     }
 
     public static void makeAir() {
-        // left
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1) {
-                    break;
-                }
-                board[i][j] = air;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N][M];
+
+        // 판의 가장자리에서 BFS 시작
+        for (int i = 0; i < N; i++) {
+            if (!visited[i][0]) {
+	q.add(new int[]{i, 0});
+	visited[i][0] = true;
+            }
+            if (!visited[i][M - 1]) {
+	q.add(new int[]{i, M - 1});
+	visited[i][M - 1] = true;
+            }
+        }
+        for (int j = 0; j < M; j++) {
+            if (!visited[0][j]) {
+	q.add(new int[]{0, j});
+	visited[0][j] = true;
+            }
+            if (!visited[N - 1][j]) {
+	q.add(new int[]{N - 1, j});
+	visited[N - 1][j] = true;
             }
         }
 
-        // right
-        for (int i = 0; i < board.length; i++) {
-            for (int j = board[i].length - 1; j >= board[i].length; j--) {
-                if (board[i][j] == 1)
-                    break;
-                board[i][j] = air;
-            }
-        }
+        // BFS를 사용하여 공기 영역 탐색
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int y = current[0];
+            int x = current[1];
+            board[y][x] = air; // 현재 위치를 공기로 표시
 
-        // up
-        for (int i = 0; i < board[0].length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[j][i] == 1)
-                    break;
-                board[j][i] = air;
-            }
-        }
+            for (int i = 0; i < 4; i++) {
+	int ny = y + dy[i];
+	int nx = x + dx[i];
 
-        // down
-        for (int i = 0; i < board[0].length; i++) {
-            for (int j = board.length - 1; j >= 0; j--) {
-                if (board[j][i] == 1)
-                    break;
-                board[j][i] = air;
+	// 판 범위 내에 있고, 방문하지 않았으며, 치즈가 아닌 경우
+	if (ny >= 0 && ny < N && nx >= 0 && nx < M && !visited[ny][nx]
+	    && board[ny][nx] != 1) {
+	    q.add(new int[]{ny, nx});
+	    visited[ny][nx] = true;
+	}
             }
         }
     }
 
-    public static boolean isBoardEmpty() {
-        boolean isEmpty = true;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] != 0 || board[i][j] != air) {
-                    isEmpty = false;
-                    break;
-                }
+
+    public static int meltCheese() {
+        int cnt = 0;
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+	if (board[i][j] == 1 && isExposedToAir(i, j)) {
+	    q.add(new int[]{i, j});
+	}
             }
         }
-        return isEmpty;
+
+        while (!q.isEmpty()) {
+            int[] temp = q.poll();
+            board[temp[0]][temp[1]] = air;
+            cnt++;
+        }
+
+        return cnt;
+    }
+
+    public static boolean isExposedToAir(int i, int j) {
+        for (int k = 0; k < 4; k++) {
+            int ny = i + dy[k];
+            int nx = j + dx[k];
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N && board[ny][nx] == air) {
+	return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBoardEmpty() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+	if (board[i][j] == 1) {
+	    return false;
+	}
+            }
+        }
+        return true;
     }
 }
