e5e026378a0b9b7bdcc770732baf4792b2248096
gundorit <packdev937@gmail.com>
2023-12-24 14:43:56 +0900

    solve (Baekjoon) : 26069

---
+++ src/baekjoon/data_structure/_26069.java
@@ -0,0 +1,34 @@
+package baekjoon.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class _26069 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Set<String> dance = new HashSet<>();
+        dance.add("ChongChong");
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String firstPerson = st.nextToken();
+            String secondPerson = st.nextToken();
+            if (dance.contains(firstPerson)) {
+	dance.add(secondPerson);
+            } else if(dance.contains(secondPerson)) {
+	dance.add(firstPerson);
+            }
+        }
+
+        System.out.println(dance.size());
+    }
+}
