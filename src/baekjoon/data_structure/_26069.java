74a9d0bb275108331b9e8ce7ccf222cca1397976
gundorit <packdev937@gmail.com>
2023-12-26 10:15:09 +0900

    solve (Baekjoon) : 26069

--- src/baekjoon/data_structure/_26069.java
+++ src/baekjoon/data_structure/_26069.java
@@ -3,9 +3,7 @@ package baekjoon.data_structure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
