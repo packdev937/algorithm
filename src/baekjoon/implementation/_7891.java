aa7a0e2f985294ba45ad17bf4948d0a4eb6b4e01
Geonwoo Pack <packdev937@gmail.com>
2024-01-18 20:32:37 +0900

    solve (Baekjoon) : 7891

---
+++ src/baekjoon/implementation/_7891.java
@@ -0,0 +1,21 @@
+package baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _7891 {
+
+    public static void main(String [] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
