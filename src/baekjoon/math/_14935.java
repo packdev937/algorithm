8b1536679a3980ec10dea5ca41fa82e428475259
Geonwoo Pack <packdev937@gmail.com>
2024-03-25 21:35:10 +0900

    solve (baekjoon) : 14935

---
+++ src/baekjoon/math/_14935.java
@@ -0,0 +1,15 @@
+package baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _14935 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String current = br.readLine();
+        System.out.println("FA");
+    }
+}
--- src/study/basic/union_find.java
+++
@@ -1,7 +0,0 @@
-package study.basic;
-
-public class union_find {
-    public static void main(String[] args) {
-        
-    }
-}
