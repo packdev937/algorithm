c0adff30ba507c897ae2d525c562f92a836cff11
Geonwoo Pack <packdev937@gmail.com>
2024-04-01 22:57:02 +0900

    solve (baekjoon) : 2914

---
+++ src/baekjoon/implementation/_2914.java
@@ -0,0 +1,20 @@
+package baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+public class _2914 {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(A*(I-1)+1));
+        bw.flush();
+        bw.close();
+    }
+}
