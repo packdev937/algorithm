d4ef9d3b8e281957f548294ac42a60a8598aacb7
Geonwoo Pack <packdev937@gmail.com>
2024-04-22 23:53:59 +0900

    solve (baekjoon) : 2460

---
+++ src/baekjoon/implementation/_2460.java
@@ -0,0 +1,21 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+public class _2460 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int human = 0;
+        int max = 0;
+        for (int i = 0; i < 10; i++) {
+            int out = sc.nextInt();
+            int in = sc.nextInt();
+            human -= out;
+            human += in;
+            if (human>max) max = human;
+        }
+        System.out.println(max);
+    }
+
+}
