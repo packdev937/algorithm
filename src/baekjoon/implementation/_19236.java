cee7330efd92f7bc537cc0ff7604ceda2401ab5f
Geonwoo Pack <packdev937@gmail.com>
2024-09-29 17:32:37 +0900

    solve (baekjoon) : 19236

--- src/baekjoon/implementation/_19236.java
+++ src/baekjoon/implementation/_19236.java
@@ -5,39 +5,38 @@ import java.io.*;
 
 class _19236 {
     static class Shark {
-        int x, y, dir, eatSum;
+        int x, y, direction, accumulatePoint;
 
         Shark(int x, int y, int dir, int eatSum) {
             this.x = x;
             this.y = y;
-            this.dir = dir;
-            this.eatSum = eatSum;
+            this.direction = dir;
+            this.accumulatePoint = eatSum;
         }
     }
 
     static class Fish {
-        int x, y, id, dir;
+        int x, y, point, direction;
         boolean isAlive;
 
         Fish(int x, int y, int id, int dir, boolean isAlive) {
             this.x = x;
             this.y = y;
-            this.id = id;
-            this.dir = dir;
+            this.point = id;
+            this.direction = dir;
             this.isAlive = isAlive;
         }
     }
 
-    // 0 부터 7 까지 순서대로 ↑, ↖, ←, ↙, ↓, ↘, →, ↗
     static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
-    static int maxSum = 0;
+    static int maxPoints = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int[][] arr = new int[4][4];
+        int[][] board = new int[4][4];
         List<Fish> fishes = new ArrayList<>();
 
         // input
@@ -48,27 +47,27 @@ class _19236 {
                 int dir = Integer.parseInt(st.nextToken()) - 1;
                 Fish f = new Fish(i, j, id, dir, true);
                 fishes.add(f);
-                arr[i][j] = id;
+                board[i][j] = id;
             }
         }
 
         // 물고기는 작은 순서부터 이동해야 하기 때문에 미리 정렬해둠
-        Collections.sort(fishes, Comparator.comparingInt(f -> f.id));
+        Collections.sort(fishes, Comparator.comparingInt(f -> f.point));
 
         // 상어는 (0, 0) 물고기를 먹고 시작하며 위치는 -1 로 표현
-        Fish firstFish = fishes.get(arr[0][0] - 1);
-        Shark shark = new Shark(0, 0, firstFish.dir, firstFish.id);
+        Fish firstFish = fishes.get(board[0][0] - 1);
+        Shark shark = new Shark(0, 0, firstFish.direction, firstFish.point);
         firstFish.isAlive = false;
-        arr[0][0] = -1;
+        board[0][0] = -1;
 
         // solution
-        dfs(arr, shark, fishes);
-        System.out.println(maxSum);
+        dfs(board, shark, fishes);
+        System.out.println(maxPoints);
     }
 
     static void dfs(int[][] arr, Shark shark, List<Fish> fishes) {
         // 현재까지 먹은 양의 최대값 갱신
-        maxSum = Math.max(maxSum, shark.eatSum);
+        maxPoints = Math.max(maxPoints, shark.accumulatePoint);
 
         // 현재 물고기 상태 복사
         List<Fish> fishCopies = copyFishes(fishes);
@@ -83,8 +82,8 @@ class _19236 {
 
         // 상어 이동
         for (int dist = 1; dist < 4; dist++) {
-            int nx = shark.x + dx[shark.dir] * dist;
-            int ny = shark.y + dy[shark.dir] * dist;
+            int nx = shark.x + dx[shark.direction] * dist;
+            int ny = shark.y + dy[shark.direction] * dist;
 
             if (nx >= 0 && nx < 4 && ny >= 0 && ny < 4 && arrCopies[nx][ny] > 0) {
                 int[][] newArr = copyArr(arrCopies);
@@ -92,7 +91,8 @@ class _19236 {
 
                 // 상어가 물고기를 잡아먹음
                 Fish targetFish = newFishes.get(arrCopies[nx][ny] - 1);
-                Shark newShark = new Shark(targetFish.x, targetFish.y, targetFish.dir, shark.eatSum + targetFish.id);
+                Shark newShark = new Shark(targetFish.x, targetFish.y, targetFish.direction, shark.accumulatePoint
+                    + targetFish.point);
                 targetFish.isAlive = false;
 
                 // 보드 업데이트
@@ -104,34 +104,34 @@ class _19236 {
         }
     }
 
-    static void moveFish(Fish fish, int[][] arr, List<Fish> fishes) {
+    static void moveFish(Fish fish, int[][] board, List<Fish> fishes) {
         if (!fish.isAlive) return;
 
         for (int i = 0; i < 8; i++) {
-            int newDir = (fish.dir + i) % 8;
+            int newDir = (fish.direction + i) % 8;
             int nx = fish.x + dx[newDir];
             int ny = fish.y + dy[newDir];
 
-            if (nx >= 0 && nx < 4 && ny >= 0 && ny < 4 && arr[nx][ny] != -1) {
+            if (nx >= 0 && nx < 4 && ny >= 0 && ny < 4 && board[nx][ny] != -1) {
                 // 물고기 이동
-                if (arr[nx][ny] == 0) {
+                if (board[nx][ny] == 0) {
                     // 빈칸으로 이동
-                    arr[fish.x][fish.y] = 0;
+                    board[fish.x][fish.y] = 0;
                     fish.x = nx;
                     fish.y = ny;
                 } else {
                     // 다른 물고기와 위치 교환
-                    Fish swapFish = fishes.get(arr[nx][ny] - 1);
+                    Fish swapFish = fishes.get(board[nx][ny] - 1);
                     swapFish.x = fish.x;
                     swapFish.y = fish.y;
-                    arr[fish.x][fish.y] = swapFish.id;
+                    board[fish.x][fish.y] = swapFish.point;
 
                     fish.x = nx;
                     fish.y = ny;
                 }
 
-                arr[nx][ny] = fish.id;
-                fish.dir = newDir;
+                board[nx][ny] = fish.point;
+                fish.direction = newDir;
                 return;
             }
         }
@@ -148,7 +148,7 @@ class _19236 {
     static List<Fish> copyFishes(List<Fish> fishes) {
         List<Fish> temp = new ArrayList<>();
         for (Fish f : fishes) {
-            temp.add(new Fish(f.x, f.y, f.id, f.dir, f.isAlive));
+            temp.add(new Fish(f.x, f.y, f.point, f.direction, f.isAlive));
         }
         return temp;
     }
