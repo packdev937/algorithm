c6ff2877f40653cefc9865166126166fbc460a20
Geonwoo Pack <packdev937@gmail.com>
2024-11-15 10:15:30 +0900

    solve (baekjoon) : 별 찍기 리팩토링

--- src/baekjoon/implementation/_2444.java
+++ src/baekjoon/implementation/_2444.java
@@ -1,33 +1,23 @@
 package baekjoon.implementation;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class _2444 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for (int i = 0; i < 2 * N - 1; i++) {
-            for (int j = 0; j < Math.abs(i - (N - 1)); j++) {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= 2 * N; i++) {
+            for(int j = 0; j < Math.abs(N-i); j++) {
                 System.out.print(" ");
             }
-            if (i < N) {
-                for (int j = 0; j < i; j++) {
-                    System.out.print("*");
-                }
+
+            for(int j = 0; j < 2 * N - 1 - 2 * Math.abs(N-i); j++) {
                 System.out.print("*");
-                for (int j = 0; j < i; j++) {
-                    System.out.print("*");
-                }
             }
-            else if (i >= N) {
-                for (int j = 0; j < (2*N-1) - i - 1; j++) {
-                    System.out.print("*");
-                }
-                System.out.print("*");
-                for (int j = 0; j < (2*N-1) - i - 1; j++) {
-                    System.out.print("*");
-                }
-            }
+
             System.out.println();
         }
     }
